364a05f0772245e2262b5edf3d8ebb3d80964045
Rohan Parkes <rparkes@email.com>
2025-11-26 18:59:46 +1100

    REstored original formatting code.

--- apihelper.py
+++ apihelper.py
@@ -4,17 +4,24 @@
 import inspect
 
 def info(obj, spacing=10, collapse=1):
-    """Print methods and doc strings.
-    Takes module, class, list, dictionary, or string."""
+    """ Print methods and doc strings.
+        Takes module, class, list, dictionary, or string.
 
-    # This is the closest I can get to the author's original intent in Py3. Dir() doesn't return all methods,
-    # and callable doesn't filter out the __< >__ methods.
-    methodlist = mlist = [n for (n, v) in inspect.getmembers(obj) if callable(v) and not n.startswith('__')]
+        Args:
+            obj (_type_): _description_
+            spacing (int, optional): _description_. Defaults to 10.
+            collapse (int, optional): _description_. Defaults to 1.
+    """
+
+    # This is the closest I can get to the author's original intent in Py3.
+    # Callable doesn't filter out the __< >__ methods.
+    methodlist = [n for (n, v) in inspect.getmembers(obj) if callable(v) and not n.startswith('__')]
 
     processfunc = (lambda s: " ".join(s.split())) if collapse else (lambda s: s)
 
-    print("\n".join([f"{method.ljust(spacing)} {processfunc(str(getattr(obj, method).__doc__))}"
-                     for method in methodlist]) )
+    print("\n".join(["%s %s" %
+                     (method.ljust(spacing), processfunc(str(getattr(obj, method).__doc__)))
+                     for method in methodlist ]))
 
 if __name__ == "__main__":
     print(info.__doc__)
