830ed3997415e83b707f3c52796f14e55880ae73
Rohan Parkes <rparkes@email.com>
2025-12-18 16:09:22 +1100

    Recreated recursive fib file.

---
+++ fib_rec.py
@@ -0,0 +1,23 @@
+"""Calculate factorial of n recursively with debug prints.
+Calculate factorial of n recursively with debug prints.
+Demonstration of recursion.
+"""
+
+
+def fib(n) -> int:
+    """Calculate factorial of n recursively with debug prints."""
+
+    print("n =", n)
+
+    if n > 1:
+
+        return n * fib(n - 1)
+
+    print("end of the line")
+
+    return 1
+
+
+if __name__ == "__main__":
+
+    print(fib(3))
